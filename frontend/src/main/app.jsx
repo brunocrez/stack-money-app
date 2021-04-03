9a87859b7a2957a2535395133d0a32643ddd2f0b
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-03 09:51:35 -0300

    footer + menu tree components

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,10 +4,15 @@ import React from 'react';
 
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
+import Footer from '../common/template/Footer';
 
 export default props => (
   <div className="wrapper">
     <Header />
     <Sidebar />
+    <div className="content-wrapper">
+      <h1>Salve!</h1>
+    </div>
+    <Footer />
   </div>
 );
