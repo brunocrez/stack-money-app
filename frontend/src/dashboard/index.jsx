424654228599ded07fb91fc70ea7e34ac2cda31f
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-03 10:12:03 -0300

    configuring routes + content and content header components

---
+++ frontend/src/dashboard/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import ContentHeader from '../common/template/ContentHeader'
+import Content from '../common/template/Content';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Dashboard" small="Versão 1.0.0" />
+        <Content>
+          Dashboard
+        </Content>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,13 +5,14 @@ import React from 'react';
 import Header from '../common/template/Header';
 import Sidebar from '../common/template/Sidebar';
 import Footer from '../common/template/Footer';
+import Routes from './routes';
 
 export default props => (
   <div className="wrapper">
     <Header />
     <Sidebar />
     <div className="content-wrapper">
-      <h1>Salve!</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
