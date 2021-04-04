ff5e9ddca0e24e31a4aa85f903473e861a893b9a
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-04 08:09:38 -0300

    organizing layout using grid and rows

--- frontend/src/dashboard/index.jsx
+++ frontend/src/dashboard/index.jsx
@@ -2,14 +2,34 @@ import React, { Component } from 'react';
 
 import ContentHeader from '../common/template/ContentHeader'
 import Content from '../common/template/Content';
-
+import ValueBox from '../common/widget/ValueBox';
+import Row from '../common/layout/Row';
 class Dashboard extends Component {
   render() {
     return (
-      <div>
+      <div>        
         <ContentHeader title="Dashboard" small="Versão 1.0.0" />
         <Content>
-          Dashboard
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="R$ 10"
+              text="Total de Créditos" />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="R$ 10"
+              text="Total de Débitos" />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="R$ 10"
+              text="Valor Consolidado" />
+          </Row>
         </Content>
       </div>
     );
