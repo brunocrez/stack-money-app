df24748c815e99bfd18c7f06e7b2480b5e270ea2
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-11 10:13:33 -0300

    configuring actions on credit list component

--- frontend/src/billingCycle/CreditList/index.jsx
+++ frontend/src/billingCycle/CreditList/index.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Field, arrayInsert, arrayRemove } from 'redux-form';
 
 import Grid from '../../common/layout/Grid';
 import InputCreditDebt from '../../common/form/InputCreditDebt';
 
 class CreditList extends Component {
+
+  add(index, item = {}) {
+    if (!this.props.readOnly) {
+      this.props.arrayInsert('billingCycleForm', 'credits', index, item);
+    }
+  }
+
+  remove(index) {
+    if (!this.props.readOnly && this.props.list.length > 1) {
+      this.props.arrayRemove('billingCycleForm', 'credits', index);
+    }
+  }
 
   renderRows() {
     const list = this.props.list || [];
@@ -22,7 +36,26 @@ class CreditList extends Component {
             component={InputCreditDebt}
             readOnly={this.props.readOnly} />
         </td>
-        <td></td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.add(idx + 1)}>
+            <i className="fa fa-plus"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => this.add(idx + 1, item)}>
+            <i className="fa fa-clone"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={() => this.remove(idx)}>
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </td>
       </tr>
     ));
   }
@@ -50,4 +83,5 @@ class CreditList extends Component {
   }
 }
 
-export default CreditList;
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
+export default connect(null, mapDispatchToProps)(CreditList);
