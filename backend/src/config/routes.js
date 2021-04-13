854bbbc9e1031be8be5b71c1a115342b90195e10
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-13 09:09:23 -0300

    adding .env files to gitignore + reconfig routes (using auth middleware)

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function (server) {
-  // define base_url
-  const router = express.Router();
-  server.use('/api', router);
 
-  // routes
+  // Protected Routes (needs JWT)
+  const protectedRoutes = express.Router();
+  server.use('/api', protectedRoutes);
+  protectedRoutes.use(auth);
+
   const BillingCycle = require('../api/billingCycle/billingCycleService');
-  BillingCycle.register(router, '/billingCycles');
+  BillingCycle.register(protectedRoutes, '/billingCycles');
+
+  // Open Routes (no need JWT)
+  const openRoutes = express.Router();
+  server.use('/oapi', openRoutes);
+
+  const authService = require('../api/user/authService');
+  openRoutes.post('/login', authService.login);
+  openRoutes.post('/signup', authService.signup);
+  openRoutes.post('/validateToken', authService.validateToken);
 }
