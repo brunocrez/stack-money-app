9374a55811cb31da675cac21ff106308ce99ea72
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-15 15:42:49 -0300

    keep just the newer version of react's project

--- frontend-old/src/auth/authTypes.js
+++
@@ -1,2 +0,0 @@
-export const TOKEN_VALIDATED = 'TOKEN_VALIDATED';
-export const USER_FETCHED = 'USER_FETCHED';
--- frontend-old/src/common/tabs/tabsTypes.js
+++
@@ -1,2 +0,0 @@
-export const TAB_SELECTED = 'TAB_SELECTED';
-export const TAB_SHOW = 'TAB_SHOW';
