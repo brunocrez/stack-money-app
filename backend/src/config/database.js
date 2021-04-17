44b5aadabf0f0b256edd9d83d961e19f1328813d
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-17 08:19:34 -0300

    fixing string connection mongodb

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/money-app';
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/money-app';
 
 module.exports = mongoose.connect(url, {
   useNewUrlParser: true,
