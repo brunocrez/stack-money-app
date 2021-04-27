66647e0d137dacfcab441da75d525e20393fa6ba
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-27 11:10:46 -0300

    billingCycle list component to functional component

--- frontend/src/billingCycle/BillingCycleList/index.jsx
+++ frontend/src/billingCycle/BillingCycleList/index.jsx
@@ -1,60 +1,56 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { getList, showUpdate, showDelete } from '../billingCycleActions';
+import { API_URL } from '../../URLs';
 
-class BillingCycleList extends Component {
+const BillingCycleList = () => {
 
-  componentDidMount() {
-    this.props.getList();
-  }
+  const [list, setList] = useState(null);
 
-  renderRows() {
-    const list = this.props.list || [];
-    return list.map(el => (
-      <tr key={el._id}>
-        <td>{el.name}</td>
-        <td>{el.month}</td>
-        <td>{el.year}</td>
-        <td>
-          <button 
-            className="btn btn-warning"
-            onClick={() => this.props.showUpdate(el)}>
-            <i className="fa fa-pencil"></i>
-          </button>
-          <button 
-            className="btn btn-danger"
-            onClick={() => this.props.showDelete(el)}>
-            <i className="fa fa-trash-o"></i>
-          </button>
-        </td>
-      </tr>
-    ))
-  }
+  useEffect(() => {
+    async function getList() {
+      const response = await axios.get(`${API_URL}/billingCycles`);
+      setList(response.data);
+    }
+    getList();
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Mês</th>
-              <th>Ano</th>
-              <th className="table-actions">Ações</th>
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Mês</th>
+            <th>Ano</th>
+            <th className="table-actions">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          { list && list.map(el => (
+            <tr key={el._id}>
+              <td>{el.name}</td>
+              <td>{el.month}</td>
+              <td>{el.year}</td>
+              <td>
+                <button 
+                  className="btn btn-warning"
+                  onClick={() => console.log('EDIT')}>
+                  <i className="fa fa-pencil"></i>
+                </button>
+                <button 
+                  className="btn btn-danger"
+                  onClick={() => console.log('DELETE')}>
+                  <i className="fa fa-trash-o"></i>
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {this.renderRows()}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-const mapStateToProps = state => ({ list: state.billingCycle.list });
-const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
+export default BillingCycleList;
